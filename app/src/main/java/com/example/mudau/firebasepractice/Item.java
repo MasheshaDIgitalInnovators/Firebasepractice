8995083ea8be234fe24415f606b98bf9b352f78b
MasheshaDIgitalInnovators <31695029+MasheshaDIgitalInnovators@users.noreply.github.com>
2017-10-30 12:58:11 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mudau/firebasepractice/Item.java
@@ -0,0 +1,29 @@
+package com.example.mudau.firebasepractice;
+
+/**
+ * Created by mudau on 10/30/2017.
+ */
+
+public class Item {
+
+/**
+ * Created by echessa on 8/27/16.
+ */
+
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
